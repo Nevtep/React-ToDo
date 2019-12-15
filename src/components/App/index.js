dc0893eab39049cc9af3f41923166bba286e0576
Tomas Corellano <core@MacBook-Pro-de-Tomas.local>
2019-12-15 17:45:16 -0300

    todo list

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Container } from '@material-ui/core'
-import MyComponent from 'components/MyComponent'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn GraphQL", isCompleted: false },
+    { text: "Read a Book", isCompleted: false },
+    { text: "Build an App", isCompleted: false }
+  ])
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const clickTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+
   return (
     <Container>
       <Box
@@ -10,9 +29,20 @@ function App() {
         display='flex'
         justifyContent='center'
         alignItems='center'
-        flexDirection='row'
+        flexDirection='column'
       >
-        <MyComponent />
+        <TodoForm addTodo={addTodo} />
+        <List component="nav">
+          {todos.map( (todo, index) => (
+            <ListItem
+              button
+              onClick={() => {clickTodo(index)}}
+              style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+            >
+              <ListItemText primary={todo.text} />
+            </ListItem>
+          ))}
+        </List>
       </Box>
     </Container>
   );
---
+++ src/components/TodoForm/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { TextField } from '@material-ui/core';
+
+function TodoForm({ addTodo }) {
+    const [value, setValue] = useState("");
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      if (!value) return;
+      addTodo(value);
+      setValue("");
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <TextField
+          type="text"
+          className="input"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+      </form>
+    );
+  }
